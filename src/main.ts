901423dadfaa2d76929691d97ca5d1b1372fcbdb
Prakash Banjade <prakashbanjade191@gmail.com>
2024-05-08 23:01:38 +0545

    Add: helmet and cors option

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,22 @@ import { AllExceptionsFilter } from './all-exceptions.filter';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { setupSwagger } from './config/swagger.config';
+import helmet from 'helmet';
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['http://client.com'],
     credentials: true,
     optionsSuccessStatus: 200,
+    preflightContinue: false, // enforce CORS policy consistently across the application's endpoints.
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   });
 
+  // Helmet helps secure your apps by setting various HTTP headers. It’s not specific to NestJS but is essential for web applications to prevent common attacks.
+  app.use(helmet());
+  
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.use(cookieParser());
